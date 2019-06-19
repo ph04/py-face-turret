509dc7a3727e8a6689036189bc6c7ca56d7e5e50
ph04 <47356461+ph04@users.noreply.github.com>
2019-06-19 15:43:03 +0200

    Create server.py

---
+++ src/server.py
@@ -0,0 +1,35 @@
+import cherrypy
+import serial
+import sys
+import time
+
+class Server():
+    @cherrypy.expose
+    def index(self, x, y):
+        self.x = list(str(x))
+        self.y = list(str(y))
+
+        print(self.x, self.y)
+        
+        # In this part you have to figure out how to connect your Rasperry to your Arduino through a serial port, it's very boring good luck.
+        # In my case it worked plugging the Arduino to the Raspberry in its USB port and the port seems to be called "ttyACM0" in my case
+        # you could try to do the same but it's easier to check out the name first.
+        self.ser = serial.Serial("/dev/" + sys.argv[1], 9600) # ttyACM0
+
+        for i in range(3):
+            self.send(x[i])
+
+        time.sleep(0.001)
+
+        for i in range(3):
+            self.send(y[i])
+
+    def send(self, src):
+        self.ser.write(src.encode())
+
+def main():
+    cherrypy.server.socket_host = "0.0.0.0"
+    cherrypy.quickstart(Server())
+
+if __name__ == "__main__":
+    main()
